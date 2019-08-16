dc58e7784c644bceae8095ec1885e0b6c09aba50
Aaron Williams <aaronrw4@yahoo.com>
2019-08-15 19:21:48 -0700

    Created Lambda People

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -64,3 +64,62 @@ class ProjectManager extends Instructor {
         console.log(`${this.name} debugs ${obj.name}'s code on ${subject}`)
     }
 }
+
+const fred = new Instructor({
+    name: 'Fred',
+    location: 'Bedrock',
+    age: 37,
+    favLanguage: 'JavaScript',
+    specialty: 'Front-end',
+    catchPhrase: `Don't forget the homies`
+  });
+
+  const worf = new Instructor({
+    name: 'Worf',
+    location: 'Kronos',
+    age: 64,
+    favLanguage: 'Python',
+    specialty: 'Network Security',
+    catchPhrase: `Todays a good day to die`
+  });
+
+
+  const flash = new ProjectManager({
+    name: 'Barry',
+    location: 'Central City',
+    age: 32,
+    favLanguage: 'Swift',
+    specialty: 'Mobile',
+    catchPhrase: `I am the fastest man alive.`,
+    gradClassName: 'Android 07',
+    favInstructor: 'Harrison Wells'
+  });
+
+  const data = new ProjectManager({
+    name: 'Data',
+    location: 'USS Enterprise',
+    age: 15,
+    favLanguage: 'C++',
+    specialty: 'AI',
+    catchPhrase: `Interesting...`,
+    gradClassName: 'Data Science 05',
+    favInstructor: 'Dr. Soong'
+  });
+
+  const aaron = new Student({
+    name: 'Aaron',
+    location: 'Oakland',
+    age: 42,
+    previousBackground: 'Teacher',
+    className: 'Webpt9',
+    favSubjects: '.Less'  
+  });
+
+  const david = new Student({
+      name: 'David',
+      location: 'Fremont',
+      age: 34,
+      previousBackground: 'Human Resources',
+      className: 'Webpt11',
+      favSubjects: 'CSS'
+  });
